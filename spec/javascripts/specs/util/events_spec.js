f4f7b78ee3c98f8cd6606abec5eb3a446d9cbadf
Phil Cockfield <phil@cockfield.net>
2011-04-04 15:00:54 -0700

    Added sub-sub event bus

---
+++ spec/javascripts/specs/util/events_spec.js
@@ -0,0 +1,29 @@
+goog.require('open.core.events');
+
+
+describe('open.core.events', function() {
+  describe('API', function() {
+    it('is exposed from [open.core]', function() {
+      expect(open.core.events).toBeDefined();
+    });
+  });
+
+  describe('event firing', function() {
+    var events;
+
+    beforeEach(function() {
+      events = open.core.events;
+    });
+
+    it('responds to a subscribed event when it is published', function() {
+      var wasFired = false;
+      events.subscribe('test:event', function() { wasFired = true; });
+
+      events.publish('test:event');
+      expect(wasFired).toBeTruthy();
+    });
+  });
+});
+
+
+
